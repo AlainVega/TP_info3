30d7b7abe0214dbedfc731f1a0554437b39bc396
Alain Vega <alainjosevz@gmail.com>
2023-12-14 23:34:46 -0300

    agregar soportes

--- tp-info3/main.js
+++ tp-info3/main.js
@@ -32,7 +32,7 @@ scene.add( eje );
 ejeMaterial = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 points = [];
 points.push( new THREE.Vector3( 0, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 0, 20 ) );
+points.push( new THREE.Vector3( 0, 0, 30 ) );
 ejeGeometria = new THREE.BufferGeometry().setFromPoints( points );
 eje = new THREE.Line( ejeGeometria, ejeMaterial );
 scene.add( eje );
@@ -78,13 +78,10 @@ let rectanguloMaterial = new THREE.MeshBasicMaterial( { color: 0x848484 } );
 const rectanguloMesh = new THREE.Mesh(rectanguloGeometria, rectanguloMaterial)
 
 const basePilar = new THREE.Object3D()
-// const basePilar2 = new THREE.Object3D()
 
 basePilar.translateY(-10)
-// basePilar2.translateY(-10)
 
 basePilar.add(rectanguloMesh)
-// basePilar2.add(rectanguloMesh)
 
 rectanguloGeometria = new THREE.BoxGeometry( 7.5, 1, 7.5 ) 
 rectanguloMaterial = new THREE.MeshBasicMaterial( { color: 0x444444 } );
@@ -93,7 +90,6 @@ const rectanguloMesh2 = new THREE.Mesh(rectanguloGeometria, rectanguloMaterial)
 rectanguloMesh2.translateY(1)
 
 basePilar.add(rectanguloMesh2)
-// basePilar2.add(rectanguloMesh2)
 
 rectanguloGeometria = new THREE.BoxGeometry( 5, 1, 5 ) 
 rectanguloMaterial = new THREE.MeshBasicMaterial( { color: 0x848484 } );
@@ -102,31 +98,29 @@ const rectanguloMesh3 = new THREE.Mesh(rectanguloGeometria, rectanguloMaterial)
 rectanguloMesh3.translateY(2)
 
 basePilar.add(rectanguloMesh3)
-// basePilar2.add(rectanguloMesh3)
 
 // Columnas (las 2 columnas que estan encima de la base y soportan la carretera)
-let columnaGeometria = new THREE.BoxGeometry( 1, 10, 1 ) 
-let columnaMaterial = new THREE.MeshBasicMaterial( { color: 0x434343 } );
+let largoColumna = 1
+let anchoColumna = 7
+let profundoColumna = 1
+
+let columnaGeometria = new THREE.BoxGeometry( largoColumna, anchoColumna, profundoColumna ) 
+let columnaMaterial = new THREE.MeshBasicMaterial( { color: 0xffffff } );
 let meshColumna = new THREE.Mesh(columnaGeometria, columnaMaterial)
 
-meshColumna.translateY(3)
+meshColumna.translateY(2 + anchoColumna/2) //donde tercer rectangulo altura + la mitad de la columna
 meshColumna.translateX(-1)
 
 basePilar.add(meshColumna)
-// basePilar2.add(meshColumna)
 
 let meshColumna2 = new THREE.Mesh(columnaGeometria, columnaMaterial)
 
-meshColumna2.translateY(3)
+meshColumna2.translateY(2 + anchoColumna/2) //donde tercer rectangulo altura + la mitad de la columna
 meshColumna2.translateX(1)
 
 basePilar.add(meshColumna2)
-// basePilar2.add(meshColumna2)
 
 basePilar.translateX(10)
-// basePilar2.translateX(-10)
-
-// const meshBasePilar = new THREE.Mesh(basePilarGeometria, basePilarMaterial)
 
 const basePilar2 = basePilar.clone()
 basePilar2.translateX(-20)
@@ -134,29 +128,70 @@ basePilar2.translateX(-20)
 scene.add(basePilar)
 scene.add(basePilar2)
 
+// Soporte
+// Base del soporte
+let largoBaseSoporte = 2
+let anchoBaseSoporte = 1
+let profundoBaseSoporte = 2
+
+let baseSoporteGeometria = new THREE.BoxGeometry( largoBaseSoporte, anchoBaseSoporte, profundoBaseSoporte ) 
+let baseSoporteMaterial = new THREE.MeshBasicMaterial( { color: 0x444444 });
+let baseSoporteMesh = new THREE.Mesh(baseSoporteGeometria, baseSoporteMaterial)
+
+// baseSoporteMesh.translateY(-5)
+
+const soporte = new THREE.Object3D()
+
+soporte.translateY(-5)
+
+soporte.add(baseSoporteMesh)
+
+// Pilar/columna del soporte
+let largoPilarSoporte = 1
+let anchoPilarSoporte = 4
+let profundoPilarSoporte = 1
+
+let pilarSoporteGeometria = new THREE.BoxGeometry( largoPilarSoporte, anchoPilarSoporte, profundoPilarSoporte ) 
+let pilarSoporteMaterial = new THREE.MeshBasicMaterial( { color: 0x848484 } );
+let pilarSoporteMesh = new THREE.Mesh(pilarSoporteGeometria, pilarSoporteMaterial)
+
+pilarSoporteMesh.translateY(anchoBaseSoporte/2 + anchoPilarSoporte/2)
+
+soporte.add(pilarSoporteMesh)
+
+scene.add(soporte)
+
+const soporte2 = soporte.clone()
+
+soporte2.translateX(3)
+
+scene.add(soporte2)
+
+// Posicion de la camara:
 const x0 = 10
 const y0 = 5
-const z0 = 15
+const z0 = 20
 camera.position.x = x0
 camera.position.y = y0
 camera.position.z = z0
 
+// Determinacion de los angulos de la posicion de la camara
 const radio = Math.sqrt( x0*x0 + y0*y0 + z0*z0 )
-let phi = Math.acos(x0/radio)
-let rho = Math.acos(z0/radio)
+let phi = Math.acos(x0/radio) // angulo en el plano XOZ
+let rho = Math.acos(z0/radio) // angulo en el plano ZOY
 
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 
 function animate() {
 	requestAnimationFrame( animate );
 
-  // Para que se mueva solo:
-  camera.position.x = radio*Math.cos(phi)
-  camera.position.z = radio*Math.sin(phi)
+  // // Para que se mueva solo:
+  // camera.position.x = radio*Math.cos(phi)
+  // camera.position.z = radio*Math.sin(phi)
   // phi += 0.01 //plano paralelo a XoZ
 
-  camera.position.z = radio*Math.cos(rho)
-  camera.position.y = radio*Math.sin(rho)
+  // camera.position.z = radio*Math.cos(rho)
+  // camera.position.y = radio*Math.sin(rho)
   // rho += 0.01 // plano paralelo a ZoY
 
   camera.lookAt(new THREE.Vector3(0, 0, 0));
